467c194af4d8cc6935b7281fe1c64159eee770da
brnydr <brianwilliamyoder@gmail.com>
2023-08-01 22:02:13 -0700

    refactor with fetch

--- src/JSONservice.js
+++ src/JSONservice.js
@@ -1,18 +1,14 @@
 export default class JSONservice {
   static getJSON() {
-    return new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      const url = `${process.env.API_URL}`;
-      request.open("GET", url, true);
-      request.send();
-      request.addEventListener("loadend", function() {
-        const response = JSON.parse(this.responseText);
-        if (this.status === 200) {
-          resolve(response);
+    return fetch(`${process.env.API_URL}`)
+      .then((response) =>{
+        if(!response) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
         } else {
-          reject({message: "no good!", code: 404});
+          return response.json();
         }
-      })
     })
+    .catch((error) => {return error});
   }}
   

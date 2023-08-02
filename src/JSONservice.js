f9be26d1445e2b53c8abd08beba997ef28197f74
brnydr <brianwilliamyoder@gmail.com>
2023-08-01 17:50:39 -0700

    add service

---
+++ src/JSONservice.js
@@ -0,0 +1,18 @@
+export default class JSONservice {
+  static getJSON() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      const url = `${process.env.API_URL}`;
+      request.open("GET", url, true);
+      request.send();
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve(response);
+        } else {
+          reject(response);
+        }
+      })
+    })
+  }}
+  

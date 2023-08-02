e7db5f41147d4590c0b32416e58c94903e40f2f3
brnydr <brianwilliamyoder@gmail.com>
2023-08-01 19:22:46 -0700

    create ui logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import JSONservice from "./JSONservice.js"
 
-//this is where UI logic lives. 
+function getUsers() {
+  let promise = JSONservice.getJSON();
+  promise.then((response) => {
+    displayUsers(response)
+  }, (error) => {
+    displayError(error)
+  });
+}
+
+function displayUsers(response) {
+  let userList =  document.getElementById("userList");
+  response.forEach((user) => {
+    let li = document.createElement("li");
+    li.innerText = user.username;
+    userList.append(li);
+    })
+  }
+  
+
+function displayError(error) {
+  let p = document.querySelector("p")
+  p.append(error.message);
+}
+
+window.addEventListener("load", getUsers());
